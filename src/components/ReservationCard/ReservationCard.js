e266c5144910c09451f00a5c05650949ac924f24
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-11 10:30:15 -0600

    Create ReservationCard component

---
+++ src/components/ReservationCard/ReservationCard.js
@@ -0,0 +1,11 @@
+import './ReservationCard.css'
+
+const ReservationCard = () => {
+  return(
+    <div className='resie-card'>
+      <h3>Card!</h3>
+    </div>
+  )
+}
+
+export default ReservationCard
